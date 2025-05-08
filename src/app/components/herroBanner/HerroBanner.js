14ea38b71e074b168cbd5e5975bf4f9cc78cfbea
littlekanine <elia.kopff3891@gmail.com>
2025-05-08 20:54:58 +0200

    modfication Read.me

--- src/app/components/herroBanner/HerroBanner.js
+++ src/app/components/herroBanner/HerroBanner.js
@@ -5,20 +5,15 @@ import Button from '../button/Button';
 const HerroBanner = () => {
 	return (
 		<div className="relative flex items-center justify-center h-screen w-full">
-			{/* Image de fond */}
 			<div className='absolute inset-0 bg-[url("/images/restau_image.jpg")] bg-cover bg-center sm:bg-top md:bg-center brightness-50 z-0'></div>
 
-			{/* Contenu */}
 			<div className="relative z-10 flex flex-col items-center justify-center h-full w-full text-center gap-4 px-4">
-				{/* Titre principal */}
 				<h1 className="flex text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-[100px] text-white w-full justify-center items-center font-instrument">
 					LE<span className="text-gold ">&nbsp;SAINT&nbsp;</span> SAUVAGE
 				</h1>
 
-				{/* Sous-titre */}
 				<h2 className="text-white font-instrument-serif text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-[25px]">Une expérience culinaire exceptionnelle à Toulouse</h2>
 
-				{/* Boutons */}
 				<div className="flex flex-row gap-2 sm:gap-4">
 					<Button
 						className={
